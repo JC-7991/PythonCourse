fe5e5283002464a8883115b431572cd3cac6e0a5
JC <jctblock@gmail.com>
2023-03-19 22:01:30 -0400

    Checkpoint

--- DailyCode/Week 16/task106.py
+++ DailyCode/Week 16/task106.py
@@ -8,13 +8,16 @@ For example, [2, 0, 1, 0] returns true while [1, 1, 0, 1] returns false.
 from typing import List
 
 def reach(arr: List[int]) -> bool:
-    
+
     length = len(arr)
-    cur_pos, last_ind = 0
+    cur_pos = 0
+    last_ind = 0
+
     while cur_pos < length:
         if cur_pos == last_ind:
             return True
         elif arr[cur_pos]:
             return False
         cur_pos += arr[cur_pos]
+
     return False
