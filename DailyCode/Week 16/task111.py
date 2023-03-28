14c85a7df40f784bdb7383b0bd0502ff53280346
JC <jctblock@gmail.com>
2023-03-28 00:04:34 -0400

    Checkpoint

--- DailyCode/Week 16/task111.py
+++ DailyCode/Week 16/task111.py
@@ -3,3 +3,5 @@ Problem:
 Given a word W and a string S, find all starting indices in S which are anagrams of W.
 For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
 '''
+
+from typing import Dict, List
