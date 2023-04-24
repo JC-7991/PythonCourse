9073d39e0c8cada893a8eba9bf9e3d40389a73e4
JC <jctblock@gmail.com>
2023-04-24 00:01:35 -0400

    Checkpoint

--- DailyCode/Week 17/task116.py
+++ DailyCode/Week 17/task116.py
@@ -6,7 +6,9 @@ That is, generate() should return a tree whose size is unbounded but finite.
 import random
 
 class Node:
+
     def __init__(self, val = 1, left = None, right = None):
+        
         self.val = val
         self._left = left
         self._right = right
