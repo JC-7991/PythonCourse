ddf57c137e2d9bcb0768b8e5e7fe52b6209d23f9
JC <jctblock@gmail.com>
2023-04-26 00:00:32 -0400

    Checkpoint

--- DailyCode/Week 17/task117.py
+++ DailyCode/Week 17/task117.py
@@ -5,6 +5,6 @@ Given a binary tree, return the level of the tree with minimum sum.
 from collections import deque
 
 class Node:
-    
-    def __init__(self):
+
+    def __init__(self, key):
         pass
