2fc3ef6ea45bdf746b9a56c82ee93e0d6ad46677
JC <jctblock@gmail.com>
2023-05-12 00:00:19 -0400

    Checkpoint

--- DailyCode/Week 17/task119.py
+++ DailyCode/Week 17/task119.py
@@ -4,3 +4,4 @@ intervals. If there are multiple smallest sets, return any of them.
 For example, given the intervals [0, 3], [2, 6], [3, 4], [6, 9], one set of numbers
 that covers all these intervals is {3, 6}.
 '''
+
