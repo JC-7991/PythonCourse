915d36668773536ce16c50cf0884498fa2e63e84
JC <jctblock@gmail.com>
2023-05-14 00:01:02 -0400

    Checkpoint

--- DailyCode/Week 17/task119.py
+++ DailyCode/Week 17/task119.py
@@ -7,5 +7,5 @@ that covers all these intervals is {3, 6}.
 
 from typing import List, Optional, Tuple
 
-def span(intervals: List[List[int]]):
+def span(intervals: List[List[int]]) -> Optional:
     pass
