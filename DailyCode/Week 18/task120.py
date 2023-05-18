42d38e4f61f9959c1d20369f1f3d1767a032a88c
JC <jctblock@gmail.com>
2023-05-18 00:01:11 -0400

    Checkpoint

---
+++ DailyCode/Week 18/task120.py
@@ -0,0 +1,15 @@
+'''
+Implement the singleton pattern with a twist. First, instead of storing one instance,
+store two instances. And in every even call of getInstance(), return the first instance
+and in every odd call of getInstance(), return the second instance.
+'''
+
+from __future__ import annotations
+
+class Twisted_Singleton:
+
+    _instance1 = None
+    _instance2 = None
+    _isInitialized = False
+
+    
