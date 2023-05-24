1df90bec780762a0ab3fd43b6609362fb43d5d8e
JC <jctblock@gmail.com>
2023-05-24 00:00:16 -0400

    Checkpoint

--- DailyCode/Week 18/task120.py
+++ DailyCode/Week 18/task120.py
@@ -20,8 +20,9 @@ class Twisted_Singleton:
     
     @staticmethod
     def initialize() -> None:
-        
+
         if not Twisted_Singleton._isInitialized:
+            
             Twisted_Singleton._instance1 = Twisted_Singleton(1)
             Twisted_Singleton._instance2 = Twisted_Singleton(2)
             Twisted_Singleton._is_initialized = True
