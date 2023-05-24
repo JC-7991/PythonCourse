3ae19d9b125740e95283a8a492167b2b46618291
JC <jctblock@gmail.com>
2023-05-24 12:44:32 -0400

    Checkpoint

---
+++ PythonCourse/TicTacToe.py
@@ -0,0 +1,37 @@
+def display_board(board):
+
+    print('\n' * 100)
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+def player_input():
+    
+    marker = ''
+
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1, choose X or O: ').upper()
+
+    if marker == 'X':
+        return ('X', 'O')
+    
+    else:
+        return ('O', 'X')
+    
+def place_marker(board, marker, position):
+    board[position] = marker
+
+def win_check(mark, board):
+    pass
+
+if __name__ == "__main__":
+    test_board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O']
+    display_board(test_board)
