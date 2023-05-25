91d30efd6fd133bbd23b6591c0b54d574fbbb47d
JC <jctblock@gmail.com>
2023-05-25 14:10:25 -0400

    Checkpoint

--- PythonCourse/TicTacToe.py
+++ PythonCourse/TicTacToe.py
@@ -36,6 +36,7 @@ def win_check(mark, board):
     (board[7] == mark and board[4] == mark and board[1] == mark) or
     (board[8] == mark and board[5] == mark and board[2] == mark) or
     (board[9] == mark and board[6] == mark and board[3] == mark) or
+    (board[7] == mark and board[5] == mark and board[3] == mark) or
     (board[4] == mark and board[5] == mark and board[6] == mark))
 
 if __name__ == "__main__":
