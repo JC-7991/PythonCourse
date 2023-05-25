f2f9e2bde6a6d298582dfeb67b1ea943f5b54d00
JC <jctblock@gmail.com>
2023-05-25 14:14:52 -0400

    Checkpoint

--- PythonCourse/TicTacToe.py
+++ PythonCourse/TicTacToe.py
@@ -43,6 +43,7 @@ def win_check(board, mark):
     (board[9] == mark and board[5] == mark and board[1] == mark))
 
 if __name__ == "__main__":
+    
     test_board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'X']
     place_marker(test_board, '$', 8)
     display_board(test_board)
