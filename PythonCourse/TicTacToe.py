706422689619e95980d1d040099dd9d1fa0a1a82
JC <jctblock@gmail.com>
2023-05-26 00:00:12 -0400

    Checkpoint

--- PythonCourse/TicTacToe.py
+++ PythonCourse/TicTacToe.py
@@ -12,7 +12,6 @@ def display_board(board):
     print('   |   |')
     print('-----------')
     print('   |   |')
-    
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
 
