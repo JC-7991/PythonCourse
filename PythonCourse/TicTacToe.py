a1f0a254a44f84d302547407fb36d38f2079084d
JC <jctblock@gmail.com>
2023-05-31 13:55:38 -0400

    Checkpoint

--- PythonCourse/TicTacToe.py
+++ PythonCourse/TicTacToe.py
@@ -64,8 +64,10 @@ def full_board_check(board):
 def player_choice(board):
 
     position = 0
+
     while position not in [1, 2, 3, 4, 5, 6, 7, 8, 9] or not space_check(board, position):
         position = int(input('Choose a position (1-9): '))
+        
     return position
 
 def replay():
