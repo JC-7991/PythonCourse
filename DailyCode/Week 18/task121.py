6e392e9b433d7f6846ae347ecb341c1e37539d08
JC <jctblock@gmail.com>
2023-05-31 00:00:16 -0400

    Checkpoint

--- DailyCode/Week 18/task121.py
+++ DailyCode/Week 18/task121.py
@@ -3,3 +3,4 @@ Given a string which we can delete at most k, return whether you can make a pali
 For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get
 'waterretaw'.
 '''
+
