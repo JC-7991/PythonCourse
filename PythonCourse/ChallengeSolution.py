ae53ced9933620c6925a4f3a62dd875cc9c386ee
JC <jctblock@gmail.com>
2023-06-15 10:50:50 -0400

    Checkpoint

--- PythonCourse/ChallengeSolution.py
+++ PythonCourse/ChallengeSolution.py
@@ -3,3 +3,6 @@ class Account():
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
+
+    def deposit(self, depAmt):
+        self.balance = self.balance + depAmt
