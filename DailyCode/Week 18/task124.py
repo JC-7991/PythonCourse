3a7956aec86a6aba9edff41d78e76be2e3077f6e
JC <jctblock@gmail.com>
2023-06-23 00:01:27 -0400

    Checkpoint

--- DailyCode/Week 18/task124.py
+++ DailyCode/Week 18/task124.py
@@ -6,3 +6,15 @@ Write a function that, given 'n', returns the number of rounds you'd expect to p
 until one coin remains.
 '''
 
+from math import log2, ceil
+
+def expectation(n: int) -> int:
+    return ceil(log2(n + 1))
+
+
+if __name__ == "__main__":
+    print(expectation(0))
+    print(expectation(1))
+    print(expectation(2))
+    print(expectation(100))
+    print(expectation(200))
