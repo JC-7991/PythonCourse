a97b9fd87ffe101fbbbd5bae41beb0fea11124fd
JC <jctblock@gmail.com>
2023-06-23 00:01:36 -0400

    Checkpoint

--- DailyCode/Week 18/task124.py
+++ DailyCode/Week 18/task124.py
@@ -11,9 +11,8 @@ from math import log2, ceil
 def expectation(n: int) -> int:
     return ceil(log2(n + 1))
 
-
 if __name__ == "__main__":
-    
+
     print(expectation(0))
     print(expectation(1))
     print(expectation(2))
