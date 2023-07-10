adf064ab6692fc4084088a5510a8ff5de5c7f737
JC <jctblock@gmail.com>
2023-07-10 11:26:37 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -8,4 +8,6 @@ values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eig
 class Card:
 
     def __init__(self, suit, rank):
-        
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
