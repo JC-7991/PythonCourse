6bb48f72a037b1696c5b043f6d6252282aac7155
JC <jctblock@gmail.com>
2023-07-10 11:24:44 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -2,3 +2,4 @@ import random
 
 suits = {'Hearts', 'Diamonds', 'Spades', 'Clubs'}
 ranks = {'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'}
+values = {'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace'}
