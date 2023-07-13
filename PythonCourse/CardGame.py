7e4cecca688c825af5ba889b3722cb0d904cabc3
JC <jctblock@gmail.com>
2023-07-13 10:41:40 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -44,6 +44,8 @@ class Player:
     def add_cards(self, new_cards):
         if type(new_cards) == type([]):
             self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
 
     def __str__(self):
         return f'Player {self.name} has {len(self.all_cards)} cards.'
