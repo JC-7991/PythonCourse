de6948048d4f945a1ac5615f1f152a1ccc80e45f
JC <jctblock@gmail.com>
2023-07-17 12:38:18 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -19,12 +19,11 @@ class Deck:
 
     def __init__(self):
 
-        self.all_cards = []
+        self.deck = []
 
         for suit in suits:
             for rank in ranks:
-                created_card = Card(suit, rank)
-                self.all_cards.append(created_card)
+                pass
 
     def shuffle(self):
         random.shuffle(self.all_cards)
