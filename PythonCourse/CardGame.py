604f905fbe6e377f18959800ca1a37c7a2ec063c
JC <jctblock@gmail.com>
2023-07-17 12:37:47 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -11,7 +11,6 @@ class Card:
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
-        #self.value = values[rank]
 
     def __str__(self):
         return self.rank + " of " + self.suit
