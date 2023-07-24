0d97ec3317fd3323e1af67197529bc50c27765bb
JC <jctblock@gmail.com>
2023-07-24 10:13:10 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -65,4 +65,9 @@ class Chips:
     def __init__(self, total = 100):
         self.total = total
         self.bet = 0
-        
+    
+    def win_bet(self):
+        pass
+
+    def lose_bet(self):
+        pass
