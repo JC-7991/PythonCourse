506a33b446c3700c0f7db31c12b5d648bf75d7d9
JC <jctblock@gmail.com>
2023-07-24 10:12:42 -0400

    Checkpoint

--- PythonCourse/CardGame.py
+++ PythonCourse/CardGame.py
@@ -60,25 +60,9 @@ class Hand:
             self.value -= 10
             self.aces -= 1
     
-'''
-class Player:
+class Chips:
 
-    def __init__(self, name):
-        self.name = name
-        self.all_cards = []
-
-    def remove_one(self):
-        return self.all_cards.pop()
-
-    def add_cards(self, new_cards):
-
-        if type(new_cards) == type([]):
-            self.all_cards.extend(new_cards)
-
-        else:
-            self.all_cards.append(new_cards)
-
-    def __str__(self):
-        return f'Player {self.name} has {len(self.all_cards)} card(s).'
-'''
-
+    def __init__(self, total = 100):
+        self.total = total
+        self.bet = 0
+        
