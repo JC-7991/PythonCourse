bfb9b28e3fb56dece4775936501a39783ea8bdd4
JC <jctblock@gmail.com>
2023-07-28 00:03:05 -0400

    Checkpoint

--- DailyCode/Week 19/task129.py
+++ DailyCode/Week 19/task129.py
@@ -4,7 +4,7 @@ Given a real number n, find the square root of n. For example, given n = 9, retu
 
 X = 10 ** (-6)
 
-def almostEqual(num1: float, num2: float):
+def almostEqual(num1: float, num2: float) -> bool:
     return num1 - X < num1 < num2 + X
 
 def getSquare(num: int):
