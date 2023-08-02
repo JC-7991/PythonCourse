355f3634e35f2bc3ce40814b352dcb2073fe41f9
JC <jctblock@gmail.com>
2023-08-02 00:00:14 -0400

    Checkpoint

--- DailyCode/Week 19/task130.py
+++ DailyCode/Week 19/task130.py
@@ -5,4 +5,3 @@ You must buy the stock before you can sell it, and you must sell the stock befor
 can buy it again.
 For example, given k = 2 and the array [5, 2, 4, 0, 1], you should return 3.
 '''
-
