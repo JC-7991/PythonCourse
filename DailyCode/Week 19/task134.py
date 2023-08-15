cd7feb6d5ae04dbf055f2c8dba7373939c8cb433
JC <jctblock@gmail.com>
2023-08-15 13:23:34 -0400

    Checkpoint

--- DailyCode/Week 19/task134.py
+++ DailyCode/Week 19/task134.py
@@ -22,8 +22,9 @@ class SparseArray:
     def __repr__(self) -> str:
 
         string = ""
-        
+
         for pos in range(self.size):
             if pos in self.arr:
                 string += f"{self.arr[pos]}, "
             else:
+                string += "0, "
