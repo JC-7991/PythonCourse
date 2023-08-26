0c94a24f6bdbffe9c184e3f9db3225c3422e3df0
JC <jctblock@gmail.com>
2023-08-26 00:00:54 -0400

    Checkpoint

---
+++ DailyCode/Week 19/task134.py
@@ -0,0 +1,59 @@
+'''
+You have a large array with most of the elements as zero.
+Use a more space-efficient data structure, SparseArray, that implements the same interface:
+init(arr, size): initialize with the original large array and size.
+set(i, val): updates index at i with val.
+get(i): gets the value at index i.
+'''
+
+from typing import List
+
+class SparseArray:
+
+    def __init__(self, arr: List[int], size: int) -> None:
+
+        self.arr = {}
+        self.size = size
+
+        for index, val in enumerate(arr):
+            if val != 0:
+                self.arr[index] = val
+
+    def __repr__(self) -> str:
+
+        string = ""
+
+        for pos in range(self.size):
+
+            if pos in self.arr:
+                string += f"{self.arr[pos]}, "
+
+            else:
+                string += "0, "
+
+        return "[" + string.rstrip(" ,") + "]"
+    
+    def set(self, pos: int, val: int) -> int:
+
+        if pos > self.size:
+            raise IndexError
+        
+        if val == 0:
+            if pos in self.arr:
+                del self.arr[pos]
+
+        else:
+            self.arr[pos] = val
+    
+    def get(self, pos: int) -> int:
+        
+        if pos > self.size:
+            raise IndexError
+        
+        if pos in self.arr:
+            return self.arr[pos]
+        
+        return 0
+    
+if __name__ == "__main__":
+    pass
