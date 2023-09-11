e960c5322b8316acfd90888fc70f74935d382b7a
JC <jctblock@gmail.com>
2023-09-11 10:46:05 -0400

    Checkpoint

--- PythonCourse/WebScrape.py
+++ PythonCourse/WebScrape.py
@@ -23,3 +23,6 @@ for page in range(1, 10):
     page_url = url + str(page)
     res = requests.get(page_url)
     soup = bs4.BeautifulSoup(res.text, 'lxml')
+    
+    for name in soup.select(".author"):
+        authors.add(name.text)
