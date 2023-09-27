a3aee8ffded5021c317b4f322f216c0b43ca1a96
JC <jctblock@gmail.com>
2023-09-27 00:04:20 -0400

    Checkpoint

--- DailyCode/Week 19/task135.py
+++ DailyCode/Week 19/task135.py
@@ -27,5 +27,5 @@ def min_path_sum_helper(node: Node) -> Tuple[int, List[int]]:
     return node.val, [node.val]
   elif left and not right:
         return (left_sum + node.val), left + [node.val]
-    elif right and not left:
+  elif right and not left:
         return (right_sum + node.val), right + [node.val]
